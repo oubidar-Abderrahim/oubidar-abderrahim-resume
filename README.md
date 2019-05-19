225f2c3e35aabfb39b44e88a8981ce63a288a62a
Abderrahim OUBIDAR <oubidar.abderrahim@gmail.com>
2019-05-19 15:19:04 +0000

    version 1.0.0

---
+++ README.md
@@ -0,0 +1,11 @@
+# Resume as an NPM packege
+
+This simple package contains my resume (or CV), that can be installed as an npm package and
+accessed via CLI.   
+
+The package uses [Inquirer.js](https://www.npmjs.com/package/inquirer) to prompt a list of
+choises for the use to choose from.  
+
+The resume comes in two languages : *English* and *French*
+
+Special thanks to [silentlad](https://github.com/silent-lad/silentlad)
---
+++ index.js
@@ -0,0 +1,126 @@
+#!/usr/bin/env node
+"use strict";
+
+var inquirer = require("inquirer");
+var PropertiesReader = require('properties-reader');
+var chalk = require("chalk");
+
+var response = chalk.bold.green;
+
+var resume;
+var resumePrompts;
+var properties;
+
+
+function main() {
+    inquirer
+    .prompt({
+        type: "list",
+        name: "language",
+        message: "Choose a language - Choisir une langue",
+        choices: ["English", "Français", "Sortir"]
+    })
+    .then(choice => {
+        if (choice.language == "English") {
+            
+            resume = require("./resume.json");
+            properties = PropertiesReader('./en.properties');
+            resumePrompts = {
+                type: "list",
+                name: "resumeOptions",
+                message: "What do you want to know about me?",
+                choices: [...Object.keys(resume), "Exit"]
+            };
+            
+            console.log("Hello, My name is Abderrahim OUBIDAR and welcome to my resume");
+            resumeHandler(resume, resumePrompts);
+        }
+        else if (choice.language == "Français") {
+
+            resume = require("./cv.json");
+            properties = PropertiesReader('./fr.properties');
+            resumePrompts = {
+                type: "list",
+                name: "resumeOptions",
+                message: "Que voulez-vous savoir sur moi?",
+                choices: [...Object.keys(resume), "Sortir"]
+            };
+            console.log("Bonjour, Je m'appelle Abderrahim OUBIDAR, bienvenu dans mon CV");
+            resumeHandler(resume, resumePrompts);
+        }
+        else {
+            return;
+        }
+    });
+}
+
+function isArray(a) {
+    return (!!a) && (a.constructor === Array);
+};
+
+function resumeHandler(resume, resumePrompts) {
+
+    var back = properties.get('choice.back');
+    var exit = properties.get('choice.exit');
+
+    inquirer.prompt(resumePrompts).then(answer => {
+        if (answer.resumeOptions == properties.get('choice.exit')) {
+            return;
+        }
+        var option = answer.resumeOptions;
+
+        if (isArray(resume[`${option}`])) {
+            displayChoises(resume[`${option}`]);
+        } else {
+            var msg ;
+            if(answer.resumeOptions == properties.get('choice.experience')) {
+                msg = properties.get('msg.experience');
+            } 
+
+            if(answer.resumeOptions == properties.get('choice.skill')) {
+                msg = properties.get('msg.skill');
+            } 
+            inquirer
+                .prompt({
+                    type: "list",
+                    name: "moreOptions",
+                    message: (msg) ? msg : (properties.get('msg.general') + answer.resumeOptions),
+                    choices: [...Object.keys(resume[`${option}`]), back, exit]
+                })
+                .then(choice => {
+                    if (choice.moreOptions == back) {
+                        resumeHandler(resume, resumePrompts);
+                    } else if(choice.moreOptions == exit) {
+                        return;
+                    } else {
+                        displayChoises(resume[`${option}`][`${choice.moreOptions}`]);
+                    }
+                });
+        }
+    });
+
+    function displayChoises(option) {
+        console.log(response("--------------------------------------"));
+        option.forEach(info => {
+            console.log(response("|   => " + info));
+        });
+        console.log(response("--------------------------------------"));
+        inquirer
+            .prompt({
+                type: "list",
+                name: "exitBack",
+                message: properties.get('msg.exitBack'),
+                choices: [back, exit]
+            })
+            .then(choice => {
+                if (choice.exitBack == back) {
+                    resumeHandler(resume, resumePrompts);
+                }
+                else {
+                    return;
+                }
+            });
+    }
+}
+
+main();
